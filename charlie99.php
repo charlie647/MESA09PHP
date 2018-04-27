041fe6c21f373c0d17e049ce38b42e49abe55744
Charlie Liu <charlie647@gmail.com>
2018-04-27 11:01:12 +0800

    0427 v2

--- charlie99.php
+++ charlie99.php
@@ -5,15 +5,21 @@
 
 <table border="1" width="100%">
     <tr>
-        <td>
+
             <?php
-                for ($i = 1; $i<=9; $i++){
-                    $r = 2 * $i;
-                    echo "2x{$i}={$r}<br>";
+                for ($j=2; $j<=5; $j++){
+                    echo '<td>';
+                    for ($i = 1; $i<=9; $i++){
+                        $r = $j * $i;
+                        echo "{$j}x{$i}={$r}<br>";
+                    }
+                echo '</td>';
+
                 }
             ?>
 
-        </td>
+    </tr>
+    
         <td>
             2x1=2<br>
             2x1=2<br>
