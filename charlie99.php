6427f7dbdf5771f7d43c50e8f547f8b5bd9e978a
Charlie Liu <charlie647@gmail.com>
2018-04-27 10:53:43 +0800

    0427 init commit

---
+++ charlie99.php
@@ -0,0 +1,29 @@
+
+
+
+
+
+<table border="1" width="100%">
+    <tr>
+        <td>
+            <?php
+                for ($i = 1; $i<=9; $i++){
+                    $r = 2 * $i;
+                    echo "2x{$i}={$r}<br>";
+                }
+            ?>
+
+        </td>
+        <td>
+            2x1=2<br>
+            2x1=2<br>
+        </td>
+        <td>
+            2x1=2<br>
+            2x1=2<br>
+        </td>
+        <td>
+            2x1=2<br>
+            2x1=2<br>
+        </td>
+
